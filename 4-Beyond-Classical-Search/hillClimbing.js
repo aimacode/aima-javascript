03516a674bedc37a1cded17c6017a2f2918b384f
norvig <peter@norvig.com>
2016-03-19 17:38:54 -0700

    Massive cleanup and refactor, some codes from chapter 4 and 6

--- 4-Beyond-Classical-Search/hillClimbing.js
+++ 4-Beyond-Classical-Search/hillClimbing.js
@@ -1,68 +1,28 @@
 /*
 Logic for the hill climber robot
 Author: Souradeep Nanda
-Date: 18-03-2016
 */
 
-var HillClimber = function(x,y,blockSize){
+var HillClimber = function(){
 	// Should the robot go left, right or stay where it is
 	this.DECISIONS = {
 		LEFT:-1,
 		RIGHT:1,
 		STAY:0
 	};
-	
-	// Store current position
-	this.current.x = x;
-	this.current.y = y;
-	this.blockSize = blockSize;
-	
+
 	// Decide where to go based on height
-	this.decide = function(left,right){
-		if(left > right){
+	this.decide = function(left,current,right){
+		if(left == right && left > current){
+			this.currentDecision = Math.random() > .5 ?
+			this.DECISIONS.LEFT: this.DECISIONS.RIGHT;
+		} else if(right == null || left > right){
 			this.currentDecision = this.DECISIONS.LEFT;
-		} else if (right > left) {
+		} else if (left == null || right > left) {
 			this.currentDecision = this.DECISIONS.RIGHT;
 		} else {
 			this.currentDecision = this.DECISIONS.STAY;
 		}
 		return this.currentDecision;
 	}
-	
-	// This function is used to calculate the intermediate
-	// values of x and y, while the robot is jumping
-	this.jump = function(t){
-		if(this.currentDecision == this.DECISIONS.LEFT)
-			t = -t;
-		
-		// Linearly interpolate x and sinosoidally y
-		var intermediate = 
-		[this.current.x + t*blockSize,
-		 this.current.y + sin((3+3*t)*3.14/2)*blockSize];
-		
-		// When jump is complete, update position
-		if(t == 1){
-			this.current.y+=blockSize;
-			if(this.currentDecision == this.DECISIONS.LEFT)
-				this.current.x-=blockSize;
-			else
-				this.current.x+=blockSize;
-		}
-		
-		return intermediate;
-	}	
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
