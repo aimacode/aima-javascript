4f5186ed7e02118f5b66e1b91cefbbb628e2e011
Chi-Hsien Yen <ha531102@gmail.com>
2017-04-03 00:10:33 -0500

    as previous commit

--- 7-Logical-Agents/c_logicalAgent.js
+++ 7-Logical-Agents/c_logicalAgent.js
@@ -1,3 +1,9 @@
+// global variables
+var world;
+var world_status = {};
+var squares;
+var gameController = {};
+
 $(document).ready(function(){
   // $.ajax({
   //   url : "logicalAgent.js",
@@ -9,12 +15,112 @@ $(document).ready(function(){
 
   // Constants
   const WUMPUS_WORLD_SIZE = 4;
+  const WUMPUS_WORLD_UI_WIDTH = 300;
+  const SQUARE_SIZE = WUMPUS_WORLD_UI_WIDTH / WUMPUS_WORLD_SIZE;
 
   // initialize UI of wumpus world
   function initWumpusWorld(){
-    console.log(WUMPUS_WORLD_SIZE);
-  }
-  initWumpusWorld();
+    world = d3.select('#wumpus_world').append('svg')
+              .attr('width', WUMPUS_WORLD_UI_WIDTH)
+              .attr('height', WUMPUS_WORLD_UI_WIDTH);
 
+    var background = world.append('rect')
+                          .classed('background', true)
+                          .attr('width', WUMPUS_WORLD_UI_WIDTH)
+                          .attr('height', WUMPUS_WORLD_UI_WIDTH);
+    
+    squares = world.selectAll('g.square').data(world_status.squares)
+                   .enter()
+                   .append('g')
+                   .attr('transform', d => 'translate(' + (d.x-1) * SQUARE_SIZE + ',' + (4-d.y) * SQUARE_SIZE + ')')
+    
+    squares_rect = squares.append('rect')
+                       .attr('width', SQUARE_SIZE)
+                       .attr('height', SQUARE_SIZE)
+                       .classed('square', true);
+
+    squares_text = squares.append('text')
+                          .text(d => '(' + d.x + ',' + d.y + ')')
+                          .attr('x', 10)
+                          .attr('y', 15)
+  }
+
+  // initialize locations of wumpus, squares, etc
+  function initWorldStatus(){
+    var squares = []
+    for (var i = 1; i <= WUMPUS_WORLD_SIZE; i++) {
+      for (var j = 1; j <= WUMPUS_WORLD_SIZE; j++) {
+        squares.push({x: i, y: j});
+      }
+    };
+    world_status.squares = squares;
+  }
+
+  // initialize game player position
+  function initPlayer(){
+    player = { 
+      x: 1,
+      y: 1
+    }
+    world_status.player = player;
+
+    player = world.selectAll('g.player').data([world_status.player])
+                  .enter()
+                  .append('g')
+                  .classed('player', true)
+                  .attr('transform', d => 'translate(' + (d.x-1) * SQUARE_SIZE + ',' + (4-d.y) * SQUARE_SIZE + ')');
+
+    player.append('circle')
+          .attr('cx', SQUARE_SIZE / 2)
+          .attr('cy', SQUARE_SIZE / 2)
+          .attr('r', 10)
+          .classed('player', true)
+  }
+
+  // local functions
+  function movePlayer(x, y) {
+    world_status.player.x = new_position.x;
+    world_status.player.y = new_position.y;
+    
+    world.select('g.player').transition()
+         .attr('transform', d => 'translate(' + (d.x-1) * SQUARE_SIZE + ',' + (4-d.y) * SQUARE_SIZE + ')');
+  }
+
+  // global functions (interfact of game)
+  gameController.playerMove = function (direction) {
+    switch (direction) {
+      case 'up':
+        offset = [0, 1];
+        break
+      case 'down':
+        offset = [0, -1];
+        break
+      case 'right':
+        offset = [1, 0];
+        break
+      case 'left':
+        offset = [-1, 0];
+        break
+      default:
+        // invalid input
+        console.log('invalid input');
+        return
+    }
+    
+    new_position = {x: world_status.player.x + offset[0], y: world_status.player.y + offset[1]}
+
+    // invalid move
+    if (new_position.x < 1 || new_position.x > WUMPUS_WORLD_SIZE || new_position.y < 1 || new_position.y > WUMPUS_WORLD_SIZE) {
+      console.log('invalid move');
+      return
+    }
+
+    movePlayer(new_position.x, new_position.y);
+  }
+
+  // enter point
+  initWorldStatus();
+  initWumpusWorld();
+  initPlayer();
 
 });
