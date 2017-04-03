26bd8aadd170baf71cc8c0f7a3fef600ef2d2963
Chi-Hsien Yen <ha531102@gmail.com>
2017-04-03 00:01:35 -0500

    wumpus ui basics, playerMove() implemented

---
+++ 7-Logical-Agents/c_logicalAgent.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  // $.ajax({
+  //   url : "logicalAgent.js",
+  //   dataType: "text",
+  //   success : function (data) {
+  //     $("#codearea").html(data);
+  //   }
+  // });
+
+  // Constants
+  const WUMPUS_WORLD_SIZE = 4;
+
+  // initialize UI of wumpus world
+  function initWumpusWorld(){
+    console.log(WUMPUS_WORLD_SIZE);
+  }
+  initWumpusWorld();
+
+
+});
