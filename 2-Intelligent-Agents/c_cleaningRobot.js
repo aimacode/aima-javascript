42c09204de5d357c2870b43e1d218d99176dbf73
Amit Patel <redblobgames@gmail.com>
2017-05-03 09:01:26 -0700

    Update ch 2 - split world, percept and action rendering

--- 2-Intelligent-Agents/c_cleaningRobot.js
+++ 2-Intelligent-Agents/c_cleaningRobot.js
@@ -58,18 +58,28 @@ function makeDiagram(selector) {
 }
 
 
-/* Render agent's percept and actions into the diagram object */
-function render(diagram, dirty, action) {
-    let perceptLabel = {false: "It's clean", true: "It's dirty"}[dirty];
-    let actionLabel = {null: 'Waiting', 'SUCK': 'Vacuuming', 'LEFT': 'Going left', 'RIGHT': 'Going right'}[action];
-    
-    diagram.perceptText.text(perceptLabel);
-    diagram.actionText.text(actionLabel);
-    
+/* Rendering functions read from the state of the world (diagram.world) 
+   and write to the state of the diagram (diagram.*). For most diagrams
+   we only need one render function. For the vacuum cleaner example, to
+   support the different styles (reader driven, agent driven) and the
+   animation (agent perceives world, then pauses, then agent acts) I've
+   broken up the render function into several. */
+
+function renderWorld(diagram) {
     for (let floorNumber = 0; floorNumber < diagram.world.floors.length; floorNumber++) {
         diagram.floors[floorNumber].attr('class', diagram.world.floors[floorNumber].dirty? 'dirty floor' : 'clean floor');
     }
     diagram.robot.style('transform', `translate(${diagram.xPosition(diagram.world.location)}px,100px)`);
+}
+
+function renderAgentPercept(diagram, dirty) {
+    let perceptLabel = {false: "It's clean", true: "It's dirty"}[dirty];
+    diagram.perceptText.text(perceptLabel);
+}
+
+function renderAgentAction(diagram, action) {
+    let actionLabel = {null: 'Waiting', 'SUCK': 'Vacuuming', 'LEFT': 'Going left', 'RIGHT': 'Going right'}[action];
+    diagram.actionText.text(actionLabel);
 }
 
 
@@ -77,6 +87,7 @@ function render(diagram, dirty, action) {
    controller is simple. Every STEP_TIME_MS milliseconds choose an
    action, simulate the action in the world, and draw the action on
    the page. */
+
 const STEP_TIME_MS = 2500;
 function makeAgentControlledDiagram() {
     let diagram = makeDiagram('#agent-controlled-diagram svg');
@@ -86,7 +97,9 @@ function makeAgentControlledDiagram() {
         let percept = diagram.world.floors[location].dirty;
         let action = reflexVacuumAgent(diagram.world);
         diagram.world.simulate(action);
-        render(diagram, percept, action);
+        renderWorld(diagram);
+        renderAgentPercept(diagram, percept);
+        renderAgentAction(diagram, action);
     }
     update();
     setInterval(update, STEP_TIME_MS);
@@ -149,13 +162,17 @@ function makeReaderControlledDiagram() {
         let percept = diagram.world.floors[diagram.world.location].dirty;
         if (nextAction !== null) {
             diagram.world.simulate(nextAction);
-            render(diagram, percept, nextAction);
+            renderWorld(diagram);
+            renderAgentPercept(diagram, percept);
+            renderAgentAction(diagram, nextAction);
             nextAction = null;
             updateButtons();
             animating = setTimeout(update, STEP_TIME_MS);
         } else {
             animating = false;
-            render(diagram, percept, null);
+            renderWorld(diagram);
+            renderAgentPercept(diagram, percept);
+            renderAgentAction(diagram, null);
         }
     }
 }
@@ -175,7 +192,9 @@ function makeTableControlledDiagram() {
         let percept = diagram.world.floors[location].dirty;
         let action = tableVacuumAgent(diagram.world, table);
         diagram.world.simulate(action);
-        render(diagram, percept, action);
+        renderWorld(diagram);
+        renderAgentPercept(diagram, percept);
+        renderAgentAction(diagram, action);
         showPerceptAndAction(location, percept, action);
     }
     update();
@@ -199,10 +218,7 @@ function makeTableControlledDiagram() {
                 let marker = d3.select(this).attr('data-input');
                 return marker == perceptMarker || marker == locationMarker;
             })
-            .transition().duration(0.1 * STEP_TIME_MS)
-            .style('background-color', colors.perceptHighlight)
-            .transition().delay(0.3 * STEP_TIME_MS).duration(0.3 * STEP_TIME_MS)
-            .style('background-color', colors.perceptBackground);
+            .style('background-color', (d) => colors.perceptHighlight);
         
         d3.selectAll('#table-controlled-diagram td')
             .style('padding', '5px')
