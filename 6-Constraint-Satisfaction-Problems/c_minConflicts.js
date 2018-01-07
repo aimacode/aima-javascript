3fcd19f402bc93c16464cfd370c6f65e162e768e
Dragneel7 <sainisurya.1@gmail.com>
2018-01-07 18:56:33 +0530

    add algorithm comparison

--- 6-Constraint-Satisfaction-Problems/c_minConflicts.js
+++ 6-Constraint-Satisfaction-Problems/c_minConflicts.js
@@ -1,15 +1,30 @@
-function setBoard(FEN,total){
+//sets the state of the random function board
+function setBoardRandom(FEN,total){
     if(total == 0){
-        document.getElementById('status').innerHTML='SOLVED';
-        describe('none','none','none','none','none');
+        document.getElementById('status-random').innerHTML='SOLVED';
+        describeRandomMinConflict('none','none','none','none','none',13);
     }
     else{
-        document.getElementById('status').innerHTML='WRONG STATE';   
+        document.getElementById('status-random').innerHTML='WRONG STATE';   
     }
-    var board = ChessBoard('board',FEN);
+    var board = ChessBoard('board-random',FEN);
+}
+
+//sets the state of min-conflict function board
+function setBoardMinConflict(FEN,total){
+    if(total == 0){
+        document.getElementById('status-minConflict').innerHTML='SOLVED';
+        describeBoardMinConflict('none','none','none','none','none');
+    }
+    else{
+        document.getElementById('status-minConflict').innerHTML='WRONG STATE';   
+    }
+    var board = ChessBoard('board-minConflict',FEN);
 }
 
 $(document).ready(function() {
-    setBoard();
-    reset();        
+    setBoardRandom();
+    setBoardMinConflict();
+    resetMinConflict();
+    resetRandomConflict();        
 });
--- 6-Constraint-Satisfaction-Problems/minConflicts.js
+++ 6-Constraint-Satisfaction-Problems/minConflicts.js
@@ -3,48 +3,114 @@ function queens(){
 	return soln;
 }
 
-var soln = queens();
+var soln_minConflict = queens();
+var soln_randomConflict = queens();
 
 var col_selected = [ 3, 0, 4, 1, 7, 2, 0, 3, 3, 1, 1, 6 ]; // gives the column to be selected 
 var row_selected = [ 5, 3, 4, 6, 3, 1, 2, 7, 6, 6, 5, 7 ]; // gives the row to be selected 	
 
-var index = 0 ; 
-function minConflicts(){
-		var conflict = conflicts(soln);
-		var total_conflicts = conflict.reduce(add, 0);
-		col = col_selected[index];
-		val = row_selected[index];
+var index = 0;
+var count = 0;
 
-		queen_conflict = []; // stores the no. of conflicts for a queen at each row
-		for(j=0;j<8;j++){
-			queen_conflict[j] = hit(col,j,soln);
-		}
-		
-		min = Math.min.apply(null, queen_conflict); // gives the minimum no. of conflicts for a queen
-		
-		row = [];
-		for(j = 0;j<8;j++){
-			if(queen_conflict[j] ==	 min){
-				row.push(j);
-			}
-			else{
-				continue;
-			}
-		}
-		prev_row = soln[col]; 
-		soln[col] = val; // change the queen's position
-		index = index+1; // increment the value to chose the next col and row
-		describe(col,queen_conflict,min,val,prev_row);
-		toFEN(soln,total_conflicts);
+//Runs update function for Min-Conflict
+var x = [];
+var intervalMinConflict; 
+function animateMinConflicts(){
+	intervalMinConflict = setInterval(function(){
+		x = minConflicts();
+		setBoardMinConflict(x[0],x[1]);},1000);
 }
 
-//describes the movement on the board
-function describe(col,queen_conflict,min,val,prev_row){
+function minConflicts(){
+	var conflict = conflicts(soln_minConflict);
+	var total_conflicts = conflict.reduce(add, 0);
+	col = col_selected[index];
+	val = row_selected[index];
+
+	queen_conflict = []; // stores the no. of conflicts for a queen at each row
+	for(j=0;j<8;j++){
+		queen_conflict[j] = hit(col,j,soln_minConflict);
+	}
+		
+	min = Math.min.apply(null, queen_conflict); // gives the minimum no. of conflicts for a queen
+		
+	row = [];
+	for(j = 0;j<8;j++){
+		if(queen_conflict[j] ==	 min){
+			row.push(j);
+		}
+		else{
+			continue;
+		}
+	}
+	prev_row = soln_minConflict[col]; 
+	soln_minConflict[col] = val; // change the queen's position
+	index = index+1; // increment the value to chose the next col and row
+	describeBoardMinConflict(col,queen_conflict,min,val,prev_row,index);
+	fen = toFEN(soln_minConflict);
+	return [fen, total_conflicts];
+}
+
+function describeBoardMinConflict(col,queen_conflict,min,val,prev_row,index){
 	document.getElementById('col').innerHTML = col ;
 	document.getElementById('conflicts').innerHTML = queen_conflict ;
 	document.getElementById('min-conflict').innerHTML = min ;
 	document.getElementById('row').innerHTML = val ;
 	document.getElementById('row-prev').innerHTML = prev_row ;
+	document.getElementById('index').innerHTML = index;
+}
+
+//Runs update function for Random-Conflict
+var  y = [];
+var intervalRandomConflict;
+function animateRandomConflicts(){
+	intervalRandomConflict = setInterval(function(){
+	y = randomConflict();
+	setBoardRandom(y[0],y[1]);},500);
+}
+
+function randomConflict(){
+	var conflict = conflicts(soln_randomConflict);
+	var total_conflicts = conflict.reduce(add,0);
+	if(total_conflicts == 0){
+		fen = toFEN(soln_randomConflict);
+		return [fen, 0];
+	}
+
+	col = Math.floor(Math.random()*8); // randomly select a col for changing
+	val = Math.floor(Math.random()*8); // randomly select a row for changing
+	
+	queen_conflict = []; // stores the no. of conflict for a queen at each row
+	for(k=0;k<8;k++){
+		queen_conflict[k] = hit(col,j,soln_randomConflict);
+	}
+
+	min = Math.min.apply(null, queen_conflict); // gives the minimum no. of conflicts for a queen
+
+	row = [];
+	for(j = 0;j<8;j++){
+		if(queen_conflict[j] ==	 min){
+			row.push(j);
+		}
+		else{
+			continue;
+		}
+	}
+	prev_row = soln_randomConflict[col];
+	soln_randomConflict[col] = val;
+	count++;
+	describeBoardRandom(col,queen_conflict,min,val,prev_row,count);
+	fen = toFEN(soln_randomConflict,);
+	return [fen, total_conflicts];
+}
+
+function describeBoardRandom(col,queen_conflict,min,val,prev_row,count){
+	document.getElementById('col-r').innerHTML = col ;
+	document.getElementById('conflicts-r').innerHTML = queen_conflict ;
+	document.getElementById('min-conflict-r').innerHTML = min ;
+	document.getElementById('row-r').innerHTML = val ;
+	document.getElementById('row-prev-r').innerHTML = prev_row ;
+	document.getElementById('count').innerHTML = count;
 }
 
 //returns an array of total no. of conflicts for each queen
@@ -74,15 +140,8 @@ function add(a,b){
 	return a+b;
 }
 
-function reset(){
-	soln = queens();
-	index = 0;
-	describe('none','none','none','none','none');
-	toFEN(soln);
-}
-
 // converts the solution into FEN which can be understood by chessboard.js
-function toFEN(soln,total_conflicts){
+function toFEN(soln){
 	fen = ['11111111','11111111','11111111','11111111','11111111','11111111','11111111','11111111'];
 	for(k=0;k<8;k++){
 		pos = soln[k];
@@ -97,9 +156,29 @@ function toFEN(soln,total_conflicts){
 			fencode = fencode+fen[0];
 		}
 	}
-	setBoard(fencode,total_conflicts);
+	//setBoardRandom(fencode,total_conflicts);
+	return fencode;
 }
 
 function replaceAt(string, index, replace) {
   return string.substring(0, index) + replace + string.substring(index + 1);
+}
+
+//resets the board state
+function resetMinConflict(){
+	clearInterval(intervalMinConflict);
+	soln_minConflict = queens();
+	index = 0;
+	describeBoardMinConflict('none','none','none','none','none',0);
+	fencode = toFEN(soln_minConflict);
+	setBoardMinConflict(fencode);
+}
+
+function resetRandomConflict(){
+	clearInterval(intervalRandomConflict);
+	soln_randomConflict = queens();
+	count = 0;
+	describeBoardRandom('none','none','none','none','none',0);
+	fencode = toFEN(soln_randomConflict);
+	setBoardRandom(fencode);
 }
